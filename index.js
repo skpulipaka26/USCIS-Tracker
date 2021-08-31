3ad8afebd71b212dfc376684943ede2d5a3b2bed
skpulipaka26 <skpulipaka@gmail.com>
2021-08-30 22:26:17 -0700

    cron task

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const axios = require('axios').default;
 const path = require('path');
+const cron = require('node-cron');
+
+const axios = require('axios').default;
 
 const cookieFile = path.join(__dirname, 'cookie.txt');
 const Cookie = fs.readFileSync(cookieFile, 'utf8');
@@ -10,19 +12,32 @@ const statusFile = path.join(__dirname, 'status.json');
 const interestedCases = ['IOE0913005289', 'IOE0913005290', 'IOE0913005291', 'IOE0913005292'];
 
 const getStatus = async () => {
-    const requests = interestedCases.map(caseNumber => {
-        return axios.get(`https://my.uscis.gov/account/case-service/api/case_status/${caseNumber}`,
-            { headers: { Cookie } });
-    });
-    const responses = await Promise.all(requests);
-    const caseStatus = responses.map(res => res.data.data);
-    const newHeader = responses[0].headers['set-cookie'];
-    fs.writeFileSync(cookieFile, `${newHeader}`);
-    return caseStatus;
+    try {
+        const requests = interestedCases.map(caseNumber => {
+            return axios.get(`https://my.uscis.gov/account/case-service/api/case_status/${caseNumber}`,
+                { headers: { Cookie } });
+        });
+        const responses = await Promise.all(requests);
+        const caseStatus = responses.map(res => res.data.data);
+        const newHeader = responses[0].headers['set-cookie'];
+        fs.writeFileSync(cookieFile, `${newHeader}`);
+        return caseStatus;
+    } catch (error) {
+        throw error;
+    }
 }
 
 
+const getAllStatus = () => {
+    getStatus().
+        then(status => {
+            console.log('status fetched');
+            fs.writeFileSync(statusFile, JSON.stringify(status));
+        })
+        .catch(err => console.err(err));
+}
 
-getStatus().then(status => {
-    fs.writeFileSync(statusFile, JSON.stringify(status));
-});
+getAllStatus();
+
+cron.schedule('0 */5 * * * *', getAllStatus);
+
