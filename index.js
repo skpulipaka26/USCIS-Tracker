54f76299a1b36c78b5e50538c7f730e81ffb7d3b
Sai Krishna Pulipaka <spulipaka@tesla.com>
2021-08-31 20:06:12 -0700

    email

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 const cron = require('node-cron');
-
+const nodemailer = require('nodemailer');
 const axios = require('axios').default;
-
 const io = require('socket.io-client');
-const socket = io('https://signalling-server-psk.herokuapp.com');
 
+const socket = io('https://signalling-server-psk.herokuapp.com');
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: "skpulipaka@gmail.com",
+        pass: "Radhasrinivas26!"
+    }
+});
 
 const cookieFile = path.join(__dirname, 'cookie.txt');
 const statusFile = path.join(__dirname, 'status.json');
-
-const Cookie = fs.readFileSync(cookieFile, 'utf8');
 
 const REQUESTS = {
     GET_STATUS: 'GET_STATUS',
@@ -21,6 +25,7 @@ const INTERESTED_CASES = ['IOE0913005289', 'IOE0913005290', 'IOE0913005291', 'IO
 
 const getStatus = async () => {
     try {
+        const Cookie = fs.readFileSync(cookieFile, 'utf8');
         const requests = INTERESTED_CASES.map(caseNumber => {
             return axios.get(`https://my.uscis.gov/account/case-service/api/case_status/${caseNumber}`,
                 { headers: { Cookie } });
@@ -32,9 +37,28 @@ const getStatus = async () => {
                 time: new Date(),
             }
         });
+        // check if the status has updated
+        const status = fs.readFileSync(statusFile, 'utf8');
+        const currentStatus = JSON.parse(status) || [];
+        currentStatus.forEach(async s => {
+            const receiptNumber = s.receiptNumber;
+            const newStatusObj = caseStatus.find(p => p.receiptNumber === receiptNumber);
+            if (newStatusObj.statusTitle !== s.statusTitle) {
+                const summary = `${s.formType}-${receiptNumber} status changed`;
+                console.log(summary);
+                // send notification here
+                const mailOptions = {
+                    from: 'skpulipaka@gmail.com',
+                    to: 'skpulipaka@gmail.com',
+                    subject: summary,
+                    html: `New Status: ${s.statusText}`,
+                    priority: 'high'
+                };
+                await transporter.sendMail(mailOptions);
+            }
+        })
         const newHeader = responses[0].headers['set-cookie'];
-        fs.writeFileSync(cookieFile, `${newHeader}`);
-        return caseStatus;
+        return { caseStatus, newHeader };
     } catch (error) {
         throw error;
     }
@@ -45,7 +69,7 @@ const getSummary = (status) => {
     try {
         return status.reduce((a, b) => {
             return `${a}\n${b.formType} - ${b.statusTitle}`
-        }, '');
+        }, '') + '\n-------------------------------------------------------';
     } catch (error) {
         return 'Error generating summary';
     }
@@ -53,14 +77,14 @@ const getSummary = (status) => {
 
 const getAllStatus = () => {
     getStatus().
-        then(status => {
-            console.log(`${getSummary(status)}\n-------------------------------------------------------`
-            );
+        then(({ caseStatus, newHeader }) => {
+            console.log(getSummary(caseStatus));
             socket.emit('response', {
                 query: REQUESTS.GET_STATUS,
-                data: JSON.parse(JSON.stringify(status)),
+                data: JSON.parse(JSON.stringify(caseStatus)),
             });
-            fs.writeFileSync(statusFile, JSON.stringify(status, null, '\t'));
+            fs.writeFileSync(cookieFile, `${newHeader}`);
+            fs.writeFileSync(statusFile, JSON.stringify(caseStatus, null, '\t'));
         })
         .catch(err => console.log(err));
 }
